15767fb2da4d4bd2e58e7f3e310c7db17e18c550
Vince Allen <vince@vinceallen.com>
2015-08-04 18:26:58 -0400

    Added calls to Spotify api.

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ var FeedParser = require("feedparser");
 var request = require("request");
 var fs = require("fs");
 var nodemailer = require('nodemailer');
+var Q = require('q');
+
+
+// TODO: check args length; return if entries missing
 
 // RSS feed url
 var feedUrl = args.length > 2 ? args[2] : "";
@@ -16,6 +20,9 @@ var titleFilter = args.length > 3 ? args[3] : "";
 
 // Use for hashtag in email subject line
 var emailHashTag = args.length > 4 ? args[4] : "";
+
+// Holds artist names
+var artistNames = [];
 
 // Holds new items.
 var newItems = [];
@@ -40,7 +47,7 @@ request(feedUrl)
         handleItem(item);
       }
     })
-    .on("end", handleEnd);
+    .on("end", handleFeedEnd);
 
 function onError(error) {
   throw error;
@@ -48,22 +55,125 @@ function onError(error) {
 
 function handleItem(item) {
 	if (!cachedItems[item.title] && item.title.search(titleFilter) != -1) {
-		cachedItems[item.title] = "true";
-    newItems.push(item.title.replace(titleFilter, '').trim());
+		
+    cachedItems[item.title] = "true";
+    
+    var exp = new RegExp(titleFilter, "i");
+    var artistName = item.title.replace(exp, "").trim();
+    artistNames.push(artistName);
+
+    /*
+    request("https://api.spotify.com/v1/search?q=" + artistName + "&type=artist", function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var resultsArtist = JSON.parse(body);
+        if (resultsArtist.artists.total) {
+          var artistID = resultsArtist.artists.items[0].id;
+                   
+          request("https://api.spotify.com/v1/artists/" + artistID + "/top-tracks?country=US", function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              var resultsTrack = JSON.parse(body);
+              if (resultsTrack.tracks.length) {
+                var topTrack = resultsTrack.tracks[0].name;
+                console.log(topTrack);
+              }
+            }
+          });
+
+        } 
+      }
+    });*/
+
 	}
 }
 
-function handleEnd() {
+function getArtistID(artistName) {
+
+    var deferred = Q.defer();
+
+    request("https://api.spotify.com/v1/search?q=" + artistName + "&type=artist", function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var resultsArtist = JSON.parse(body);
+        if (resultsArtist.artists.total) {
+          var artistID = resultsArtist.artists.items[0].id;
+          deferred.resolve({
+            "artistName": artistName,
+            "artistID": artistID
+          });
+        } else {
+          deferred.resolve("");
+        } 
+      }
+    });
+
+    return deferred.promise;
+}
+
+function handleGetArtistIDs(results) {
+  var promises = []; 
+  var l = results.length;
+  for (var i = 0; i < l; i++) {
+    if (results[i]) {
+      promises.push(getTopTrack(results[i].artistID, results[i].artistName));
+    }
+  }
+  var allPromises = Q.all(promises);
+  allPromises.
+    then(handleGetTopTracks); 
+
+}
+
+function getTopTrack(artistID, artistName) {
+    var deferred = Q.defer();
+
+          request("https://api.spotify.com/v1/artists/" + artistID + "/top-tracks?country=US", function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+              var resultsTrack = JSON.parse(body);
+              if (resultsTrack.tracks.length) {
+                var topTrack = resultsTrack.tracks[0].name;
+                console.log(topTrack);
+                deferred.resolve({
+                  "artistName": artistName,
+                  "artistID": artistID,
+                  "topTrack": topTrack
+                });
+              } else {
+                deferred.resolve("");
+              }
+            }
+          });
+
+    return deferred.promise;
+}
+
+function handleGetTopTracks(results) {
+
+  var l = results.length;
+  for (var i = 0; i < l; i++) {
+    if (results[i]) {
+      console.log(results[i]);
+    }
+  } 
+  emailItems(results);
+}
+
+function handleFeedEnd() {
   
   str = JSON.stringify(cachedItems);
 
   fs.writeFile(CACHED_ITEMS_FILENAME, str, function(error) {
     if (error) throw error;
   });
-  
-  if (newItems.length) {
-    emailItems(newItems);
+
+  var promises = [];
+  var l = artistNames.length;
+  for (var i = 0; i < l; i++) {
+    promises.push(getArtistID(artistNames[i]));
   }
+
+  var allPromises = Q.all(promises);
+  allPromises.
+    then(handleGetArtistIDs);
+  
 }
 
 function emailItems(items) {
@@ -81,7 +191,8 @@ function emailItems(items) {
       from: config.email_addr, // sender address 
       to: config.email_recipe, // list of receivers 
       subject: emailHashTag, // Subject line 
-      text: items[i] // plaintext body 
+      text: items[i].topTrack, // plaintext body
+      html: items[i].artistName 
     }
 
     transporter.sendMail(mailOptions, function(error, info) {
